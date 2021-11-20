9c9d6d1da734715ea4aee220e78cbedaf51e0e03
victorlobato2000 <victorsilva123190@gmail.com>
2021-11-20 11:45:24 -0300

    Adicionando Email model e StatusEmail enum

---
+++ src/main/java/com/victorlobato/email/enums/StatusEmail.java
@@ -0,0 +1,6 @@
+package com.victorlobato.email.enums;
+
+public enum StatusEmail {
+    SENT,
+    ERROR;
+}
---
+++ src/main/java/com/victorlobato/email/models/Email.java
@@ -0,0 +1,30 @@
+package com.victorlobato.email.models;
+
+import lombok.AllArgsConstructor;
+import com.victorlobato.email.enums.StatusEmail;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Email implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+}
