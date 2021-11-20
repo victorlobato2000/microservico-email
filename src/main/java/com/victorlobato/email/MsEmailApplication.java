a597e027307903be8357d33260d550159b7fe822
victorlobato2000 <victorsilva123190@gmail.com>
2021-11-20 09:40:15 -0300

    Criando aplicação com Spring Boot com dependências necessárias

---
+++ src/main/java/com/victorlobato/email/MsEmailApplication.java
@@ -0,0 +1,13 @@
+package com.victorlobato.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorlobato/email/MsEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorlobato.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
