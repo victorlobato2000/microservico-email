3f56e121395824c077a53ae39d2e43c3fdd931ab
victorlobato2000 <victorsilva123190@gmail.com>
2021-11-20 11:52:34 -0300

    Adicionando EmailController e EmailService

--- src/main/java/com/victorlobato/email/controllers/EmailController.java
+++ src/main/java/com/victorlobato/email/controllers/EmailController.java
@@ -1,4 +1,30 @@
 package com.victorlobato.email.controllers;
 
+import com.victorlobato.email.models.Email;
+import com.victorlobato.email.services.EmailService;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.victorlobato.email.dto.EmailDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+
+@RestController
 public class EmailController {
+
+    @Autowired
+    EmailService emailService;
+
+    @PostMapping("/sending-email")
+    public ResponseEntity<Email> sendingEmail(@RequestBody @Valid EmailDto emailDto){
+        Email email = new Email();
+        BeanUtils.copyProperties(emailDto, email); // responsável por transformar emailDto em emailModel
+        emailService.sendEmail(email);
+        return new ResponseEntity<>(email, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/victorlobato/email/services/EmailService.java
+++ src/main/java/com/victorlobato/email/services/EmailService.java
@@ -1,4 +1,40 @@
 package com.victorlobato.email.services;
 
+import com.victorlobato.email.enums.StatusEmail;
+import com.victorlobato.email.models.Email;
+import com.victorlobato.email.repositories.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
 public class EmailService {
+
+    @Autowired
+    EmailRepository emailRepository;
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public Email sendEmail(Email email) {
+        email.setSendDateEmail(LocalDateTime.now());
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(email.getEmailFrom());
+            message.setTo(email.getEmailTo());
+            message.setSubject(email.getSubject());
+            message.setText(email.getText());
+            emailSender.send(message);
+
+            email.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e){
+            email.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(email);
+        }
+    }
 }
