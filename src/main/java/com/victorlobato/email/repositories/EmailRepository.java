142140887ec2b86e7c5b83246c563e15ffeaf032
victorlobato2000 <victorsilva123190@gmail.com>
2021-11-20 11:47:26 -0300

    Adicionando EmailRepository

---
+++ src/main/java/com/victorlobato/email/repositories/EmailRepository.java
@@ -0,0 +1,9 @@
+package com.victorlobato.email.repositories;
+
+import com.victorlobato.email.models.Email;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmailRepository extends JpaRepository<Email, Long> {
+}
